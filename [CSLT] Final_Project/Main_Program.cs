bfd46fdf576e95168a319fb53b9660bdcb13ddb5
DacienNguyeen <daciennguyeen1904@gmail.com>
2024-12-08 16:45:38 +0700

    Add project files.

---
+++ [CSLT] Final_Project/Main_Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _CSLT__Final_Project
+{
+    internal class Main_Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("Welcome to CashLand (example app name)!");
+            Console.WriteLine("==========Menu of actions==========");
+        }
+    }
+}
