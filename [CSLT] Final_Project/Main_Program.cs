45cc6958c760f3b697ccb3d648ddc613dd2f3c75
DacienNguyeen <daciennguyeen1904@gmail.com>
2024-12-09 14:49:08 +0700

    starting

--- [CSLT] Final_Project/Main_Program.cs
+++ [CSLT] Final_Project/Main_Program.cs
@@ -12,12 +12,23 @@ namespace _CSLT__Final_Project
         static void Main(string[] args)
         {
             DisplayMenu();
+            double income = GetIncome();
+            Console.WriteLine(income);
         }
 
         private static void DisplayMenu()
         {
             Console.WriteLine("Welcome to CashLand (example app name)!");
             Console.WriteLine("==========Menu of actions==========");
+        }
+
+        static double GetIncome()
+        {
+            Console.WriteLine("Input your income: ");
+            double income = double.Parse(Console.ReadLine());
+            //constraint: income is not negative, no charracter -> tryparse until correct
+
+            return income;
         }
     }
 }
